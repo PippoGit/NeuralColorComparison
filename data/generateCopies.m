88a8341153e42ab642254607b9c1b78fd97eecef
PippoGit <filipscotto@gmail.com>
2018-11-05 12:12:52 +0100

    totally random copies tested (not working)

--- data/generateCopies.m
+++ data/generateCopies.m
@@ -7,7 +7,6 @@ function copies = generateCopies(master)
     for idx=1:settings.nCopies
         for idy=1:settings.dsSize
             copies(idx).spectra(idy, :) = master(idy, :)*unifrnd(settings.nMin, settings.nMax);
-            %copies(idx).spectra = master*unifrnd(settings.nMin, settings.nMax);
         end
     end
 end
