3634df145e0de2a8133cb8e67e724ca2e66f1a07
PippoGit <filipscotto@gmail.com>
2018-11-21 10:15:48 +0100

    fixed pathname

--- brain/nccFis.m
+++ brain/nccFis.m
@@ -1,6 +1,6 @@
 function [fis] = nccFis()
 %PREPAREFUZZYINPUT Summary of this function goes here
 %   Detailed explanation goes here
-fis = readfis("nccMamdani.fis");
+fis = readfis('nccMamdani.fis');
 end
 
